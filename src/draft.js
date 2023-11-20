2647207a385cffaffcbeaaf360c0bfa5ab1f4f81
VanoKi <kirsvano@gmail.com>
2023-11-20 15:55:45 +0300

    ROT13 complete

--- src/draft.js
+++ src/draft.js
@@ -1,11 +1,21 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const ic = require('node-icecream')();
 
-function formatTime(minutes, seconds) {
-  // return `${hourse < 10 ? '0' : ''}${hourse}:${minutes < 10 ? '0' : ''}${minutes}`
-  return `${minutes.toString().padStart(2, 0)}:${seconds
-    .toString()
-    .padStart(2, 0)}`;
+function encodeToRot13(str) {
+  const abc = 'abcdefghijklmnopqrstuvwxyz';
+  const abcUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let ans = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (abc.includes(str[i])) {
+      // eslint-disable-next-line no-unused-vars
+      ans += abc[(abc.indexOf(str[i]) + 13) % 26];
+    } else if (abcUpper.includes(str[i])) {
+      ans += abcUpper[(abcUpper.indexOf(str[i]) + 13) % 26];
+    } else {
+      ans += str[i];
+    }
+  }
+  return ans;
 }
 
-ic(formatTime(5, 30));
+ic(encodeToRot13('hello'));
--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -517,8 +517,21 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const abc = 'abcdefghijklmnopqrstuvwxyz';
+  const abcUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let ans = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (abc.includes(str[i])) {
+      // eslint-disable-next-line no-unused-vars
+      ans += abc[(abc.indexOf(str[i]) + 13) % 26];
+    } else if (abcUpper.includes(str[i])) {
+      ans += abcUpper[(abcUpper.indexOf(str[i]) + 13) % 26];
+    } else {
+      ans += str[i];
+    }
+  }
+  return ans;
 }
 
 /**
